a8300998daa84b52d96da68bb66425bc92a26ff1
DilaNurfadila <nurfadila1523@gmail.com>
2024-02-01 16:11:43 +0700

    first commit

---
+++ day_30/javascript-sandbox/02-arrays-and-objects/04-array-challenge/script.js
@@ -0,0 +1,48 @@
+// My solve
+// Challenge 1
+// const arr = [1, 2, 3, 4, 5];
+
+// arr.push(6);
+// arr.unshift(0);
+// arr.reverse();
+
+// console.log(arr);
+
+// Challenge 2
+// let x;
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = [5, 6, 7, 8, 9, 10];
+
+// x = arr2.slice(1);
+// x = [...arr1, ...x];
+
+// console.log(x);
+
+// ==========================================
+
+// Challenge 1
+const arr = [1, 2, 3, 4, 5];
+
+// arr.reverse();
+// arr.push(0);
+// arr.unshift(6);
+
+arr.push(6);
+arr.unshift(0);
+arr.reverse();
+
+// Result: [6,5,4,3,2,1,0]
+
+// Challenge 2
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+
+// Solution 1
+const arr3 = arr1.slice(0, 4).concat(arr2);
+
+// Solution 2
+const arr4 = [...arr1, ...arr2];
+arr4.splice(4, 1);
+
+console.log(arr4);
